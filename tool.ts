74e3a1039a47e219b13852215160bf5f47e7e0a3
Frank Behrens <fbehrens@gmail.com>
2025-03-05 15:38:57 +0100

    write steps.json

--- tool.ts
+++ tool.ts
@@ -3,7 +3,7 @@ import { generateText, tool } from "ai";
 // import { openai } from "@ai-sdk/openai";
 // const model = openai("gpt-4.5-previ÷ew");
 import { anthropic } from "@ai-sdk/anthropic";
-import { getTranscript } from "./api.ts";
+import { getTranscript } from "./yt.ts";
 
 const model = anthropic("claude-3-7-sonnet-20250219");
 
@@ -18,8 +18,7 @@ const transcript = tool({
 });
 
 async function summarise(video: string) {
-  const { text } = await generateText({
-    // steps
+  const { text, steps } = await generateText({
     model,
     tools: {
       transcript,
@@ -27,8 +26,10 @@ async function summarise(video: string) {
     maxSteps: 5, // allow up to 5 steps
     prompt: `Please summarize the video ${video} ?`,
   });
+  await Deno.writeTextFile("out/steps.json", JSON.stringify(steps, null, 2));
   return text;
 }
-
-console.log(await summarise(Deno.args[0]));
+if (import.meta.main) {
+  console.log(await summarise(Deno.args[0]));
+}
 // deno  --allow-env --allow-net tool.ts https://youtu.be/9vDLIlB1Iro\?feature\=shared
